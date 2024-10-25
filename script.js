9787eca6734d2a52ee29ded9204d7fe6d6cfdea4
Jehu Zepeda-Silva <jehuzepeda@yahoo.com>
2024-10-25 11:50:57 -0700

    added score board

--- script.js
+++ script.js
@@ -1,17 +1,85 @@
+const changeDOM = {
+  setAttributes: function(element, attributes) {
+    for (let key in attributes) {
+      element.setAttribute(key, attributes[key]);
+    }
+  },
+  oSVG: function() {
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const svgAttributes = {
+      "xmlns": "http://www.w3.org/2000/svg",
+      "height": "130px", 
+      "viewBox": "0 -960 960 960",
+      "width": "110px",
+      "fill": "black", 
+      "stroke": "currentColor", 
+      "stroke-width": "60"
+    };
+    this.setAttributes(svg, svgAttributes);
+    const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    const circleAttributes = {
+      "cx": "480",
+      "cy": "-480", 
+      "r": "400", 
+      "fill": "yellow"
+    }
+    this.setAttributes(circle, circleAttributes);
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    const pathAttributes = {
+      "d": "M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"
+    }
+    this.setAttributes(path, pathAttributes);
+    svg.appendChild(circle);
+    svg.appendChild(path);
+    return svg;
+  },
+  xSVG: function() {
+    const svg = document.createElementNS("http://www.w3.org/2000/svg","svg");
+    const svgAttributes = {
+      "xmlns": "http://www.w3.org/2000/svg",
+      "height": "130px",
+      "viewBox": "0 -960 960 960",
+      "width": "130px",
+      "fill": "black",
+      "stroke":"currentColor",
+      "stroke-width": "50",
+      "stroke-linecap": "round",
+      "stroke-linejoin": "round"
+    };
+    this.setAttributes(svg, svgAttributes);
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    const pathAttributes = {
+      "d": "M256-200L200-256 424-480 200-704l56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"
+    }
+    this.setAttributes(path, pathAttributes);
+    svg.appendChild(path)
+    return svg;
+  }
+}
+
 const cachedDOM = {
+  change: changeDOM,
   convertToID: function(row, col) {
     const rowLength = 3;
     return (row * rowLength) + col;
   },
   getColsDOM: document.querySelectorAll(".cols"),
+  getScoreBoard: document.querySelector(".score h3"),
   getBoardCell: function(row, col) {
     const id = this.convertToID(row, col);
     const cell = this.getColsDOM[id];
     return cell;
   },
-  render: function(row, col, marker) {
+  render: function(row, col, marker, scoreMSG) {
     const cell = this.getBoardCell(row, col);
-    cell.textContent = marker;
+    if (marker === "x") {
+      const x = this.change.xSVG();
+      cell.appendChild(x);
+    } else {
+      const o = this.change.oSVG();
+      cell.appendChild(o);
+    }
+    this.getScoreBoard.textContent = scoreMSG;
   }
 }
 
@@ -86,6 +154,7 @@ function createPlayer(name, mark) {
 
 const play = {
   game: gameboard, 
+  score: null,
   player1: null,
   player2: null,
   setPlayer1: function(name, mark) {
